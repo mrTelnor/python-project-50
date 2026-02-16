1ee9d2619011651e21d313881825522007878214
Никита Волков <mrtelnor@gmail.com>
2026-02-16 22:09:00 +0300

    Шаг 6 проека "Вычислитель отличий (Python)" upd 01

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -7,11 +7,16 @@ def get_path(filename):
     return Path(__file__).parent / "test_data" / filename
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     file1 = get_path("file1.json")
     file2 = get_path("file2.json")
     expected = get_path("expected.txt").read_text()
-    
+
+    result = generate_diff(file1, file2)
+
+    assert result == expected
+
+
 def test_generate_diff_yaml():
     file1 = get_path("file1.yml")
     file2 = get_path("file2.yml")
